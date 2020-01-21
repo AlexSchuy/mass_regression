3924420c6f7999000073e36ca04fe249bd5806d1
Alex Schuy <alexjschuy@gmail.com>
2020-01-21 08:55:25 -0800

    Update Wlnu with target=nu case.

--- mass_regression/training/data.py
+++ mass_regression/training/data.py
@@ -10,6 +10,17 @@ import definitions
 from common import utils, validate
 
 
+def calc_Wm(NUz_reco, df):
+    NUE_reco = (df['NUx_reco']**2 + df['NUy_reco']**2 + NUz_reco**2)**0.5
+    LE_reco = (df['Lx_reco']**2 + df['Ly_reco']**2 +
+               df['Lz_reco']**2 + df['Lm_reco']**2)**0.5
+    WE_reco = NUE_reco + LE_reco
+    Wz_reco = df['Lz_reco'] + NUz_reco
+    Wm_reco = (WE_reco**2 - df['Wx_reco']**2 -
+               df['Wy_reco']**2 - Wz_reco**2)**0.5
+    return Wm_reco
+
+
 def get_jigsaw(dataset='Wlnu', target='W'):
     data_path = definitions.SAMPLES_DIR / dataset
 
